f29ec372452317746774b5d5acabff42d35c5c7d
juanca-dev <juancarlos17.mf@gmail.com>
2023-03-03 00:04:31 -0500

    Tipos de Datos

---
+++ main.py
@@ -0,0 +1,11 @@
+''' Son los primero script en python'''
+
+#Saludos en python
+print('Hola Juan')
+print('Bienvenidos a este nuevo reto de aprender este lenguaje de programación.')
+
+#Operaciones
+print(5 + 4)
+print(5 - 4)
+print(5 / 4)
+print(5 * 4)
---
+++ tipo_de_Datos.py
@@ -0,0 +1,32 @@
+'''Tipo de Datos'''
+
+#string o cadena de texto
+# ejemplo 
+nombre ="Jesús"
+nombre ="Jehova"
+#Integer o enteros
+# ejemplo 
+edad = 33
+edad = 55
+#flotantes o decimales
+#ejemplo
+peso_kg = 66.50
+saldo = 12.52 
+# boolen (verdadero - o falso)
+# ejemplo
+resucito = True
+77 >= 100
+False
+
+
+'''Ejemplo Real'''
+#string
+cliente = "ShpStar"
+#int
+codigo = 123
+# float
+Monto_a_pagar = 25.20
+#boolean
+deudas = True
+
+print(cliente)
